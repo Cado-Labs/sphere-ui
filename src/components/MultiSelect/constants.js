e6256ae199d63e4620432c5a9de6fad60709bca1
Sergey Andreev <sergey.and@okwork.io>
2021-10-13 16:24:50 +0300

    feat: added multiselec component

---
+++ src/components/MultiSelect/constants.js
@@ -0,0 +1,15 @@
+const LOCALES_BUTTONS_SET = {
+  ru: {
+    selectAll: "Выбрать все",
+    deselectAll: "Убрать все",
+
+  },
+  en: {
+    selectAll: "Select All",
+    deselectAll: "Deselect All",
+  },
+}
+
+export {
+  LOCALES_BUTTONS_SET,
+}
