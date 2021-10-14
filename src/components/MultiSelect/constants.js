bad583a3afd37011fe29d0d5d039a5ccbeaaa0df
Sergey Andreev <elenik1994@gmail.com>
2021-10-14 15:14:17 +0300

    SPH-2: Мультиселект (#4)
    * feat: added multiselec component
    
    * fix: change multiselect name methods
    
    Co-authored-by: Sergey Andreev <elenik1994@gmail.com>

---
+++ src/components/MultiSelect/constants.js
@@ -0,0 +1,15 @@
+const LOCALES_BUTTONS_SET = {
+  ru: {
+    selectAll: "Выбрать все",
+    clearAll: "Очистить",
+
+  },
+  en: {
+    selectAll: "Select All",
+    clearAll: "Clear",
+  },
+}
+
+export {
+  LOCALES_BUTTONS_SET,
+}
---
+++ src/components/MultiSelect/index.js
@@ -0,0 +1,83 @@
+import React, { useRef } from "react"
+import { MultiSelect as Select } from "primereact/multiselect"
+import { locale } from "primereact/api"
+
+import { LOCALES_BUTTONS_SET } from "./constants"
+
+export const MultiSelect = React.forwardRef(({
+  options,
+  value,
+  onChange,
+  filter,
+  className,
+  placeholder,
+  optionDisabled,
+  optionLabel,
+  optionValue,
+  inputId,
+  dropdownIcon,
+  maxSelectedLabels,
+  panelClassName,
+  panelStyle,
+  style,
+}, ref) => {
+  const multiselectRef = useRef(ref)
+
+  const getRef = () => {
+    return ref || multiselectRef
+  }
+
+  const handleSelectAll = event => {
+    return getRef()?.current?.onSelectAll(event)
+  }
+
+  const handleClearAll = () => {
+    const newValue = { value: [] }
+
+    onChange(newValue)
+  }
+
+  const renderHeader = () => {
+    return filter ? null : <React.Fragment />
+  }
+
+  const renderFooter = () => {
+    const translations = LOCALES_BUTTONS_SET[locale().locale]
+
+    return (
+      <div className="p-buttonset flex flex-row justify-content-center">
+        <button className="p-button" onClick={handleSelectAll}>
+          {translations.selectAll}
+        </button>
+        <button className="p-button" onClick={handleClearAll}>
+          {translations.clearAll}
+        </button>
+      </div>
+    )
+  }
+
+  return (
+    <Select
+      ref={getRef()}
+      options={options}
+      optionDisabled={optionDisabled}
+      optionLabel={optionLabel}
+      optionValue={optionValue}
+      value={value}
+      onChange={onChange}
+      filter={filter}
+      selectAll={false}
+      showSelectAll={false}
+      panelHeaderTemplate={renderHeader()}
+      panelFooterTemplate={renderFooter()}
+      className={className}
+      placeholder={placeholder}
+      inputId={inputId}
+      dropdownIcon={dropdownIcon}
+      maxSelectedLabels={maxSelectedLabels}
+      panelClassName={panelClassName}
+      panelStyle={panelStyle}
+      style={style}
+    />
+  )
+})
