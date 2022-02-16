71233c74be6aee930381bb35b19026da675ee173
Sergey Andreev <sergey.and@okwork.io>
2022-02-16 14:53:57 +0300

    fixed version checked & translate

--- storybook/components/Table.js
+++ storybook/components/Table.js
@@ -3,14 +3,12 @@ import { useTranslation } from "react-i18next"
 
 import { Title } from "./Title"
 
-const I18N_PREFIX = "components.table"
-
 Table.defaultProps = {
   description: [],
 }
 
 export function Table ({ description }) {
-  const { t } = useTranslation("translation", { keyPrefix: I18N_PREFIX })
+  const { t } = useTranslation()
 
   const translationsDescription = () => {
     return description.map(row => {
@@ -24,10 +22,10 @@ export function Table ({ description }) {
     return (
       <div className="card">
         <UI.DataTable value={translationsDescription()} showGridlines stripedRows size="small">
-          <UI.Column field="name" header={t("columns.name")} className="font-normal" />
-          <UI.Column field="type" header={t("columns.type")} className="font-light" />
-          <UI.Column body={renderDefault} header={t("columns.default")} className="font-light" />
-          <UI.Column field="description" header={t("columns.description")} className="font-light" />
+          <UI.Column field="name" header={t("components.table.columns.name")} className="font-light" />
+          <UI.Column field="type" header={t("components.table.columns.type")} className="font-light" />
+          <UI.Column body={renderDefault} header={t("components.table.columns.default")} className="font-light" />
+          <UI.Column field="description" header={t("components.table.columns.description")} className="font-light" />
         </UI.DataTable>
       </div>
     )
@@ -35,7 +33,7 @@ export function Table ({ description }) {
 
   const renderTitle = () => {
     return (
-      <Title>{t("title")}</Title>
+      <Title>{t("components.table.title")}</Title>
     )
   }
 
