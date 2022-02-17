481d53510f64476f8183e2267051e78e729ff9ec
tysky <dtysky51@gmail.com>
2022-02-17 13:57:06 +0300

    simplify props passing

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,36 +2,12 @@ import React from "react"
 import { Button as PrimeButton } from "primereact/button"
 
 export const Button = React.forwardRef(({
-  label,
-  icon,
-  iconPos,
-  tooltip,
-  tooltipOptions,
-  disabled,
-  loading,
-  loadingIcon,
-  children,
   // omitting badge props cause there is another way to add a badge in a button (see Badge stories)
   /* eslint-disable no-unused-vars */
   badge,
   badgeClassName,
   /* eslint-enable no-unused-vars */
-  ...buttonProps
+  ...props
 }, ref) => {
-  return (
-    <PrimeButton
-      ref={ref}
-      label={label}
-      icon={icon}
-      iconPos={iconPos}
-      tooltip={tooltip}
-      tooltipOptions={tooltipOptions}
-      disabled={disabled}
-      loading={loading}
-      loadingIcon={loadingIcon}
-      {...buttonProps}
-    >
-      {children}
-    </PrimeButton>
-  )
+  return <PrimeButton ref={ref} {...props} />
 })
