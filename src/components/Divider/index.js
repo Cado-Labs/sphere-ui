5e16725b4168c6a96bf5cee02a8f36fe594dfae9
Dmitry Tysky <dtysky51@gmail.com>
2022-02-24 18:10:56 +0300

    add Divider component (#50)

---
+++ src/components/Divider/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Divider as PrimeDivider } from "primereact/divider"
+
+export const Divider = React.forwardRef(({
+  align,
+  layout,
+  type,
+  style,
+  className,
+  children,
+}, ref) => {
+  return (
+    <PrimeDivider
+      ref={ref}
+      align={align}
+      layout={layout}
+      type={type}
+      style={style}
+      className={className}
+    >
+      {children}
+    </PrimeDivider>
+  )
+})
