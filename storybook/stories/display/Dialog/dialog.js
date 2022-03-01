52842c830699f42d7b4eb9d66eaf3c345c7a5f7b
tysky <dtysky51@gmail.com>
2022-03-01 11:36:35 +0300

    add dialog component

---
+++ storybook/stories/display/Dialog/dialog.js
@@ -0,0 +1,128 @@
+/* eslint-disable max-len */
+import { Dialog, Button } from "@cadolabs/sphere-ui"
+
+import i18n from "@i18n"
+
+const I18N_PREFIX = "stories.dialog"
+
+const code = `
+function DialogExample () {
+  const [displayBasic, setDisplayBasic] = React.useState(false)
+  const [displayBasic2, setDisplayBasic2] = React.useState(false)
+  const [displayResponsive, setDisplayResponsive] = React.useState(false)
+
+  const dialogFuncMap = {
+    displayBasic: setDisplayBasic,
+    displayBasic2: setDisplayBasic2,
+    displayResponsive: setDisplayResponsive,
+  }
+
+  const onClick = (name, position) => {
+    dialogFuncMap[name](true)
+
+    if (position) {
+      setPosition(position)
+    }
+  }
+
+  const onHide = name => {
+    dialogFuncMap[name](false)
+  }
+
+  const renderFooter = name => {
+    return (
+      <div>
+        <Button label="No" icon="pi pi-times" onClick={() => onHide(name)} className="p-button-text" />
+        <Button label="Yes" icon="pi pi-check" onClick={() => onHide(name)} autoFocus />
+      </div>
+    )
+  }
+
+  return (
+    <div className="p-card s-container">
+      <h3>Basic</h3>
+      <Button label="Show" icon="pi pi-external-link" onClick={() => onClick("displayBasic")} className="mr-2" />
+      <Dialog header="Header" visible={displayBasic} style={{ width: "50vw" }} footer={renderFooter("displayBasic")} onHide={() => onHide("displayBasic")}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </Dialog>
+
+      <Button label="Long Content" icon="pi pi-external-link" onClick={() => onClick("displayBasic2")} />
+      <Dialog header="Header" visible={displayBasic2} style={{ width: "50vw" }} footer={renderFooter("displayBasic2")} onHide={() => onHide("displayBasic2")}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.</p>
+        <br />
+        <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+          dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+          qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,
+          quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
+          vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        <br />
+        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,
+          similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
+          cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+          eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+        <br />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.</p>
+        <br />
+        <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+          dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+          qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,
+          quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
+          vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        <br />
+        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,
+          similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
+          cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+          eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+        <br />
+      </Dialog>
+
+      <h3>Responsive</h3>
+      <Button label="Show" icon="pi pi-external-link" onClick={() => onClick("displayResponsive")} />
+      <Dialog header="Header" visible={displayResponsive} onHide={() => onHide("displayResponsive")} breakpoints={{ "960px": "75vw" }} style={{ width: "50vw" }} footer={renderFooter("displayResponsive")}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </Dialog>
+    </div>
+  )
+}
+`
+
+const onClickParams = [
+  { name: "event", description: "Browser event" },
+]
+
+export const dialog = {
+  header: "Dialog",
+  content: i18n.t(`${I18N_PREFIX}.content`),
+  code,
+  scope: { Dialog, Button },
+  descriptionProps: [
+    { name: "id", type: "string", description: `${I18N_PREFIX}.props.id` },
+    { name: "header", type: "any", description: `${I18N_PREFIX}.props.header` },
+    { name: "footer", type: "any", description: `${I18N_PREFIX}.props.footer` },
+    { name: "visible", type: "boolean", default: false, description: `${I18N_PREFIX}.props.visible` },
+    { name: "contentStyle", type: "object", description: `${I18N_PREFIX}.props.contentStyle` },
+    { name: "contentClassName", type: "string", description: `${I18N_PREFIX}.props.contentClassName` },
+    { name: "style", type: "object", description: `${I18N_PREFIX}.props.style` },
+    { name: "className", type: "string", description: `${I18N_PREFIX}.props.className` },
+    { name: "showHeader", type: "boolean", default: false, description: `${I18N_PREFIX}.props.showHeader` },
+    { name: "baseZIndex", type: "number", default: 100, description: `${I18N_PREFIX}.props.baseZIndex` },
+    { name: "breakpoints", type: "object", description: `${I18N_PREFIX}.props.breakpoints` },
+  ],
+  eventDescriptionProps: [
+    { name: "onHide", description: `${I18N_PREFIX}.events.onHide` },
+    { name: "onShow", description: `${I18N_PREFIX}.events.onShow` },
+    { name: "onClick", params: onClickParams, description: `${I18N_PREFIX}.events.onClick` },
+    { name: "onMaskClick", params: onClickParams, description: `${I18N_PREFIX}.events.onMaskClick` },
+
+  ],
+}
