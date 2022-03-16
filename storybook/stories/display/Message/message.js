e7fa26e5ac62b277d0247017f599a108f1ba9ff4
tysky <dtysky51@gmail.com>
2022-03-16 15:49:43 +0300

    fix message story

--- storybook/stories/display/Message/message.js
+++ storybook/stories/display/Message/message.js
@@ -8,61 +8,59 @@ const I18N_PREFIX = "stories.message"
 const code = `
 function MessageExample () {
   return (
-    <div>
-      <div className="p-card s-container">
-        <h3>Inline Message</h3>
-        <p>Message component is used to display inline messages mostly within forms.</p>
-        <div className="grid">
-          <div className="col-12 md:col-3">
-            <Message severity="info" text="Message Content" />
-          </div>
-          <div className="col-12 md:col-3">
-            <Message severity="success" text="Message Content" />
-          </div>
-          <div className="col-12 md:col-3">
-            <Message severity="warn" text="Message Content" />
-          </div>
-          <div className="col-12 md:col-3">
-            <Message severity="error" text="Message Content" />
-          </div>
-        </div>
-
-        <h3>Validation Message</h3>
-        <div className="formgroup-inline mb-2">
-          <label htmlFor="username1" className="p-sr-only">Username</label>
-          <InputText id="username1" placeholder="Username" className="p-invalid mr-2" />
-          <Message severity="error" text="Username is required" />
-        </div>
-        <div className="formgroup-inline">
-          <label htmlFor="email" className="p-sr-only">email</label>
-          <InputText id="email" placeholder="Email" className="p-invalid mr-2" />
-          <Message severity="error" />
-        </div>
-
-        <h3>Form Layout</h3>
-        <div className="field p-fluid w-3">
-          <label htmlFor="username2">Username</label>
-          <InputText id="username2" aria-describedby="username-help" className="p-invalid mr-2" />
-          <small id="username-help" className="p-error">Username is not available.</small>
-        </div>
-
-        <h3>Inline Message with custom content</h3>
+    <div className="p-card s-container">
+      <h3>Inline Message</h3>
+      <p>Message component is used to display inline messages mostly within forms.</p>
+      <div className="grid">
         <div className="col-12 md:col-3">
-          <Message
-            severity="info"
-            content={
-              <div className="m-1">
-                <div className="font-bold text-center text-xl">Message title</div>
-                <div className="flex gap-2 align-items-center mt-2">
-                  <img alt="info message image" className="border-circle" src="https://picsum.photos/70" />
-                  <div>
-                    This is an information message!
-                  </div>
+          <Message severity="info" text="Message Content" />
+        </div>
+        <div className="col-12 md:col-3">
+          <Message severity="success" text="Message Content" />
+        </div>
+        <div className="col-12 md:col-3">
+          <Message severity="warn" text="Message Content" />
+        </div>
+        <div className="col-12 md:col-3">
+          <Message severity="error" text="Message Content" />
+        </div>
+      </div>
+
+      <h3>Validation Message</h3>
+      <div className="formgroup-inline mb-2">
+        <label htmlFor="username1" className="p-sr-only">Username</label>
+        <InputText id="username1" placeholder="Username" className="p-invalid mr-2" />
+        <Message severity="error" text="Username is required" />
+      </div>
+      <div className="formgroup-inline">
+        <label htmlFor="email" className="p-sr-only">email</label>
+        <InputText id="email" placeholder="Email" className="p-invalid mr-2" />
+        <Message severity="error" />
+      </div>
+
+      <h3>Form Layout</h3>
+      <div className="field p-fluid w-3">
+        <label htmlFor="username2">Username</label>
+        <InputText id="username2" aria-describedby="username-help" className="p-invalid mr-2" />
+        <small id="username-help" className="p-error">Username is not available.</small>
+      </div>
+
+      <h3>Inline Message with custom content</h3>
+      <div className="col-12 md:col-3">
+        <Message
+          severity="info"
+          content={
+            <div className="m-1">
+              <div className="font-bold text-center text-xl">Message title</div>
+              <div className="flex gap-2 align-items-center mt-2">
+                <img alt="info message image" className="border-circle" src="https://picsum.photos/70" />
+                <div>
+                  This is an information message!
                 </div>
               </div>
-            }
-          />
-        </div>
+            </div>
+          }
+        />
       </div>
     </div>
   )
@@ -97,7 +95,7 @@ export const message = {
           <li className="line-height-3">success</li>
           <li className="line-height-3">info</li>
           <li className="line-height-3">warn</li>
-          <li className="line-height-3">info</li>
+          <li className="line-height-3">error</li>
         </ul>
       </div>
     </div>
