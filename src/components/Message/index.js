6acf4ca347e471212ae4ca1402fe64647024ea47
tysky <dtysky51@gmail.com>
2022-03-21 15:08:45 +0300

    update Message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Message as PrimeMessage } from "primereact/message"
+import { classNames as cn } from "primereact/utils"
 
 export const Message = React.forwardRef(({
   id,
@@ -9,12 +10,16 @@ export const Message = React.forwardRef(({
   text,
   content,
 }, ref) => {
+  const messageClassName = cn(className, {
+    "p-inline-message-secondary": severity === "secondary",
+    "p-inline-message-help": severity === "help",
+  })
   return (
     <PrimeMessage
       ref={ref}
       id={id}
       style={style}
-      className={className}
+      className={messageClassName}
       severity={severity}
       text={text}
       content={content}
