e3795a7e90ae43089309ab40bbd61e6440a231f7
Dmitry Tysky <dtysky51@gmail.com>
2022-03-25 14:47:32 +0300

    add InputGroup layout (#81)

---
+++ storybook/stories/layouts/InputGroup/inputGroup.stories.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { InputGroup } from "./InputGroup"
+
+export default {
+  title: "Layouts / InputGroup",
+}
+
+export const InputGroupStory = context => <InputGroup {...context} />
+
+InputGroupStory.storyName = "InputGroup"
