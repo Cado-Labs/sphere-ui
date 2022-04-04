69b398bf85ffeacbd1df45adc72a5485da4d9345
tysky <dtysky51@gmail.com>
2022-04-04 16:54:44 +0300

    add templating story

--- storybook/stories/table/TreeTable/Documentation/documentation.js
+++ storybook/stories/table/TreeTable/Documentation/documentation.js
@@ -17,56 +17,29 @@ function TreeTableExample () {
           key: "0-0",
           data: { name: "React", size: "25kb", type: "Folder" },
           children: [
-            {
-              key: "0-0-0",
-              data: { name: "react.app", size: "10kb", type: "Application" },
-            },
-            {
-              key: "0-0-1",
-              data: { name: "native.app", size: "10kb", type: "Application" },
-            },
-            {
-              key: "0-0-2",
-              data: { name: "mobile.app", size: "5kb", type: "Application" },
-            },
+            { key: "0-0-0", data: { name: "react.app", size: "10kb", type: "Application" } },
+            { key: "0-0-1", data: { name: "native.app", size: "10kb", type: "Application" } },
+            { key: "0-0-2", data: { name: "mobile.app", size: "5kb", type: "Application" } },
           ],
         },
-        {
-          key: "0-1",
-          data: { name: "editor.app", size: "25kb", type: "Application" },
-        },
-        {
-          key: "0-2",
-          data: { name: "settings.app", size: "50kb", type: "Application" },
-        },
+        { key: "0-1", data: { name: "editor.app", size: "25kb", type: "Application" } },
+        { key: "0-2", data: { name: "settings.app", size: "50kb", type: "Application" } },
       ],
     },
     {
       key: "1",
       data: { name: "Cloud", size: "20kb", type: "Folder" },
       children: [
-        {
-          key: "1-0",
-          data: { name: "backup-1.zip", size: "10kb", type: "Zip" },
-        },
-        {
-          key: "1-1",
-          data: { name: "backup-2.zip", size: "10kb", type: "Zip" },
-        },
+        { key: "1-0", data: { name: "backup-1.zip", size: "10kb", type: "Zip" } },
+        { key: "1-1", data: { name: "backup-2.zip", size: "10kb", type: "Zip" } },
       ],
     },
     {
       key: "2",
       data: { name: "Desktop", size: "150kb", type: "Folder" },
       children: [
-        {
-          key: "2-0",
-          data: { name: "note-meeting.txt", size: "50kb", type: "Text" },
-        },
-        {
-          key: "2-1",
-          data: { name: "note-todo.txt", size: "100kb", type: "Text" },
-        },
+        { key: "2-0", data: { name: "note-meeting.txt", size: "50kb", type: "Text" } },
+        { key: "2-1", data: { name: "note-todo.txt", size: "100kb", type: "Text" } },
       ],
     },
     {
@@ -77,24 +50,15 @@ function TreeTableExample () {
           key: "3-0",
           data: { name: "Work", size: "55kb", type: "Folder" },
           children: [
-            {
-              key: "3-0-0",
-              data: { name: "Expenses.doc", size: "30kb", type: "Document" },
-            },
-            {
-              key: "3-0-1",
-              data: { name: "Resume.doc", size: "25kb", type: "Resume" },
-            },
+            { key: "3-0-0", data: { name: "Expenses.doc", size: "30kb", type: "Document" } },
+            { key: "3-0-1", data: { name: "Resume.doc", size: "25kb", type: "Resume" } },
           ],
         },
         {
           key: "3-1",
           data: { name: "Home", size: "20kb", type: "Folder" },
           children: [
-            {
-              key: "3-1-0",
-              data: { name: "Invoices", size: "20kb", type: "Text" },
-            },
+            { key: "3-1-0", data: { name: "Invoices", size: "20kb", type: "Text" } },
           ],
         },
       ],
@@ -107,33 +71,22 @@ function TreeTableExample () {
           key: "4-0",
           data: { name: "Spanish", size: "10kb", type: "Folder" },
           children: [
-            {
-              key: "4-0-0",
-              data: { name: "tutorial-a1.txt", size: "5kb", type: "Text" },
-            },
-            {
-              key: "4-0-1",
-              data: { name: "tutorial-a2.txt", size: "5kb", type: "Text" },
-            },
+            { key: "4-0-0", data: { name: "tutorial-a1.txt", size: "5kb", type: "Text" } },
+            { key: "4-0-1", data: { name: "tutorial-a2.txt", size: "5kb", type: "Text" } },
           ],
         },
         {
           key: "4-1",
           data: { name: "Travel", size: "15kb", type: "Text" },
           children: [
-            {
-              key: "4-1-0",
-              data: { name: "Hotel.pdf", size: "10kb", type: "PDF" },
-            },
-            {
-              key: "4-1-1",
-              data: { name: "Flight.pdf", size: "5kb", type: "PDF" },
-            },
+            { key: "4-1-0", data: { name: "Hotel.pdf", size: "10kb", type: "PDF" } },
+            { key: "4-1-1", data: { name: "Flight.pdf", size: "5kb", type: "PDF" } },
           ],
         },
       ],
     },
   ]
+
 
   const [expandedKeys, setExpandedKeys] = React.useState({})
 
