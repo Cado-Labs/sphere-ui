bc065c0d4f1072f03c5dc662b328d7269229f154
tysky <dtysky51@gmail.com>
2022-10-21 19:10:34 +0300

    update storybook

--- storybook/stories/form/DatePicker/datePicker.js
+++ storybook/stories/form/DatePicker/datePicker.js
@@ -117,6 +117,7 @@ export const datePicker = {
     { name: "mask", type: "string", default: null, description: `${I18N_PREFIX}.props.mask` },
     { name: "minDate", type: "Date", default: null, description: `${I18N_PREFIX}.props.minDate` },
     { name: "maxDate", type: "Date", default: null, description: `${I18N_PREFIX}.props.maxDate` },
+    { name: "startCalendarDate", type: "Date", default: null, description: `${I18N_PREFIX}.props.startCalendarDate` },
     { name: "className", type: "string", description: `${I18N_PREFIX}.props.className` },
     { name: "style", type: "object", description: `${I18N_PREFIX}.props.style` },
     { name: "inputClassName", type: "string", description: `${I18N_PREFIX}.props.inputClassName` },
