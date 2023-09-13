2230079072ebfda963d7500ff46fb3f01c8c5880
tysky <dtysky51@gmail.com>
2023-09-13 10:24:22 +0300

    add money separator

--- src/components/Money/index.js
+++ src/components/Money/index.js
@@ -2,10 +2,10 @@ import React, { Fragment } from "react"
 
 import { Number } from "../Number"
 
-const Money = ({ money: [amount, currency], precision = 2, ...options }) => {
+const Money = ({ money: [amount, currency], precision = 2, moneySeparator = " ", ...options }) => {
   return (
     <Fragment>
-      <Number value={amount} {...options} precision={precision} /> {currency}
+      <Number value={amount} {...options} precision={precision} />{moneySeparator}{currency}
     </Fragment>
   )
 }
