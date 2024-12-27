1c9b9a976cd851d2d98a4ecb3a64e48cabb87c8d
ivan.ego <ivan.ego@cadolabs.io>
2024-12-27 13:12:46 +0200

    fix: custom react-wrapper, single component chunk

--- src/components/EmojiPicker/index.js
+++ src/components/EmojiPicker/index.js
@@ -1,20 +1,40 @@
 import React from "react"
+import { Picker } from "emoji-mart"
 import data from "@emoji-mart/data"
-import Picker from "@emoji-mart/react"
 
 import translations from "./translations"
-
-// because it has broken and unnecessary
 const EXCLUDED_EMOJIS = ["relaxed"]
 
-export const EmojiPicker = ({
-  language = translations.en,
-  ...props
-}) => (
-  <Picker
-    data={data}
-    i18n={translations[language]}
-    exceptEmojis={EXCLUDED_EMOJIS}
-    {...props}
-  />
-)
+// @emoji-mart/react has a bug described in https://github.com/missive/emoji-mart/issues/812
+// it`s a custom react-wrapper with fix from https://github.com/missive/emoji-mart/pull/958
+export const EmojiPicker = props => {
+  const language = props?.language || "en"
+  const propsWithExtras = {
+    data,
+    i18n: translations[language],
+    exceptEmojis: EXCLUDED_EMOJIS,
+    ...props,
+  }
+
+  const ref = React.useRef(null)
+  const instance = React.useRef(null)
+  if (instance.current) {
+    instance.current.update(propsWithExtras)
+  }
+
+  React.useEffect(() => {
+    const PickerElement = window?.customElements.get("em-emoji-picker")
+    if (PickerElement) {
+      instance.current = new PickerElement({ ...propsWithExtras, ref })
+      return
+    }
+    instance.current = new Picker({ ...propsWithExtras, ref })
+
+    return () => {
+      instance.current = null
+    }
+  }, [])
+  return React.createElement("div", { ref })
+}
+
+export default EmojiPicker
