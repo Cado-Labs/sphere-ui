e178aaa9cbea33e8a89911284c78430d061cef67
Sergey Andreev <elenik1994@gmail.com>
2025-04-22 14:05:41 +0300

    fix: focus filter input when dropdown panel opens (#208)
    * fix: focus filter input when dropdown panel opens
    * fix: add dropdown collection to manage close state

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useRef } from "react"
 import { Dropdown as PrimeDropdown } from "primereact/dropdown"
 import { locale } from "primereact/api"
 import { classNames as cn } from "primereact/utils"
@@ -9,6 +9,21 @@ const EMPTY_MESSAGE = {
   ru: "Ничего не найдено",
   en: "Nothing found",
 }
+
+// Collection to track opened dropdowns
+const collections = (() => {
+  const dropdowns = []
+
+  return {
+    set: ref => dropdowns.push(ref),
+    hide: ref => dropdowns.forEach(dropdown => {
+      if (ref.getInput() !== dropdown.getInput()) {
+        dropdown.hide()
+      }
+    }),
+    remove: ref => dropdowns.filter(dropdown => dropdown.getInput() !== ref.getInput()),
+  }
+})()
 
 export const Dropdown = React.forwardRef(({
   id,
@@ -48,15 +63,35 @@ export const Dropdown = React.forwardRef(({
   onFilter,
   ...props
 }, ref) => {
+  const localRef = useRef(null)
+  const dropdownRef = ref || localRef
+
   const emptyMessage = EMPTY_MESSAGE[locale().locale]
   const dropdownClassName = cn(className, "w-full")
   const filteredTooltipOptions = filterTooltipOptions(tooltipOptions)
   const hasFilter = filter ?? shouldFilterSelectOptions(options)
   const dataAttributes = pickDataAttributes(props)
 
+  const handleShow = event => {
+    collections.hide(dropdownRef?.current)
+    collections.set(dropdownRef?.current)
+
+    onShow?.(event)
+
+    if (hasFilter) {
+      const searchInput = dropdownRef?.current?.getOverlay()?.querySelector(".p-dropdown-filter")
+      searchInput?.focus()
+    }
+  }
+
+  const handleHide = () => {
+    collections.remove(dropdownRef?.current)
+    onHide?.()
+  }
+
   return (
     <PrimeDropdown
-      ref={ref}
+      ref={dropdownRef}
       id={id}
       name={name}
       value={value}
@@ -92,8 +127,8 @@ export const Dropdown = React.forwardRef(({
       onBlur={onBlur}
       onMouseDown={onMouseDown}
       onContextMenu={onContextMenu}
-      onShow={onShow}
-      onHide={onHide}
+      onShow={handleShow}
+      onHide={handleHide}
       onFilter={onFilter}
       {...dataAttributes}
     />
