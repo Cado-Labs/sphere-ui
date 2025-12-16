ef3fc0f89eb82db2b2b5dbfc8cf99fa717f64e62
Sergey Andreev <elenik1994@gmail.com>
2025-12-16 10:30:05 +0300

    feat(Money): add roundingMode prop (#215)
    New feature:
    - Added support for the roundingMode string parameter ("ROUND_DOWN", "ROUND_UP", etc.)
    - Implemented the internal ROUNDING_MAP mapping, which maps string values ​​to the BigNumber rounding enum.
    - Set the default value to ROUND_DOWN.
    - The Money and Number components no longer require the use of bignumber.js on the consumer side.

--- src/components/Number/index.js
+++ src/components/Number/index.js
@@ -1,20 +1,37 @@
 import BigNumber from "bignumber.js"
 
+import { ROUNDING_MAP } from "./rounding"
+
 const DEFAULT_PRECISION = 2
 const DEFAULT_DELIMITER = ","
 const DEFAULT_SEPARATOR = "\xA0"
+const DEFAULT_ROUNDING = "ROUND_DOWN"
 
-export const Number = ({ value, enforcePrecision = true, ...options }) => {
-  const instance = new BigNumber(value)
-  const format = { groupSize: 3 }
+export const Number = ({
+  value,
+  enforcePrecision = true,
+  precision = DEFAULT_PRECISION,
+  roundingMode = DEFAULT_ROUNDING,
+  delimiter = DEFAULT_DELIMITER,
+  separator = DEFAULT_SEPARATOR,
+}) => {
+  const bnRounding =
+    ROUNDING_MAP[roundingMode] ?? ROUNDING_MAP[DEFAULT_ROUNDING]
 
-  const precision = options.precision == null ? DEFAULT_PRECISION : options.precision
-  format.decimalSeparator = options.delimiter == null ? DEFAULT_DELIMITER : options.delimiter
-  format.groupSeparator = options.separator == null ? DEFAULT_SEPARATOR : options.separator
+  const instance = new BigNumber(value).decimalPlaces(
+    precision,
+    bnRounding,
+  )
+
+  const format = {
+    groupSize: 3,
+    decimalSeparator: delimiter,
+    groupSeparator: separator,
+  }
 
   const string = instance.toFormat(precision, undefined, format)
 
   return enforcePrecision
     ? string
-    : string.replace(new RegExp(`(\\${DEFAULT_DELIMITER}\\d+)0+`), "$1")
+    : string.replace(new RegExp(`(\\${delimiter}\\d+)0+`), "$1")
 }
